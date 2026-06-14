8e826b5b082411bf4460aba5a438924310f4f125
igareok921-debug <igareok921-debug@users.noreply.github.com>
2026-06-14 09:11:18 +0100

    Fix canonical domain redirects

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,6 @@ const nextConfig = {
     deviceSizes: [360, 414, 640, 768, 1024, 1280, 1536],
     imageSizes: [48, 64, 96, 128, 256, 384, 512],
     formats: ["image/webp"]
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/ro",
-        permanent: false
-      }
-    ];
   }
 };
 
